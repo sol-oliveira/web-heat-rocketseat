4b8f8d7642a96ba06b74e77ad48f0b3af58bb22d
Sol <sol.oliveira@elevenfinancial.com>
2021-10-24 17:19:04 -0300

    aula-2: get messages back

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,42 +1,47 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data);
+        })
+    }, []);
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Neque porro quisquam est qui dolorem ipsum</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/diego3g.png" alt="uisque euismod" />
-                        </div>
-                        <span>Quisque euismod</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Neque porro quisquam est qui dolorem ipsum</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/diego3g.png" alt="uisque euismod" />
-                        </div>
-                        <span>Quisque euismod</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Neque porro quisquam est qui dolorem ipsum</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/diego3g.png" alt="uisque euismod" />
-                        </div>
-                        <span>Quisque euismod</span>
-                    </div>
-                </li>
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>
+                                {message.text}
+                            </p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImg}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name || "Sol Oliveira"}</span>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
